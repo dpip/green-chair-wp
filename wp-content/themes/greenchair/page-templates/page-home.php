c2eae6cde98a90c08dc5980544c3def38587d4c2
Rebecca Elmo <belmo@centerline.net>
2018-07-13 13:36:23 -0400

    updated styles and images for the homepage

--- wp-content/themes/greenchair/page-templates/page-home.php
+++ wp-content/themes/greenchair/page-templates/page-home.php
@@ -9,7 +9,7 @@ get_header(); ?>
         <?php $backgroundimage = get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>
         <div class="jumbotron" style="background-image: url(<?php echo $backgroundimage ?>);">
             <div class="container">
-                <h1 class="text-center my-5"><?php the_title(); ?></h1>
+                <h1 class="text-center my-3"><?php the_title(); ?></h1>
                 <div class="text-center text-container">
                     <?php the_content(); ?>
                     <a href="" class="btn btn-primary my-3">Give today</a>
@@ -18,8 +18,8 @@ get_header(); ?>
         </div>
         <div id="features">
             <div class="container py-4">
-                <div class="row">
-                    <div class="col">
+                <div class="row text-center">
+                    <div class="col-xs-12 col-sm-12 col-md-3 feature">
                         <div class="inner">
                             <img src="<?php bloginfo('template_directory'); ?>/assets/images/motheranddaughter.jpg" />
                             <h6>Donate Homeware</h6>
@@ -27,7 +27,7 @@ get_header(); ?>
                             <a href="" class="btn btn-primary">Learn more</a>
                         </div>
                     </div>
-                    <div class="col">
+                    <div class="col-xs-12 col-sm-12 col-md-3 feature">
                         <div class="inner">
                             <img src="<?php bloginfo('template_directory'); ?>/assets/images/eventimage.jpg" />
                             <h6>Volunteer</h6>
@@ -35,7 +35,7 @@ get_header(); ?>
                             <a href="" class="btn btn-primary">Get started</a>
                         </div>
                     </div>
-                    <div class="col">
+                    <div class="col-xs-12 col-sm-12 col-md-3 feature">
                         <div class="inner">
                             <img src="<?php bloginfo('template_directory'); ?>/assets/images/ladies.jpg" />
                             <h6>Donate</h6>
@@ -43,7 +43,7 @@ get_header(); ?>
                             <a href="" class="btn btn-primary">Donate Now</a>
                         </div>
                     </div>
-                    <div class="col">
+                    <div class="col-xs-12 col-sm-12 col-md-3 feature">
                         <div class="inner">
                             <img src="<?php bloginfo('template_directory'); ?>/assets/images/bluechair.jpg" />
                             <h6>Tag Sale Fundraiser</h6>
@@ -57,15 +57,62 @@ get_header(); ?>
         <div id="about">
             <div class="container">
                 <div class="row">
-                    <div class="col">
-                        The Green Chair Project is a non-profit organization that collects donated furnishings for people transitioning from homelessness, crisis or disasters.  
-
-We partner with over 55 agencies, shelters and nonprofits to provide essential furnishings for referred clients to live sustainably in their new homes.
+                    <div class="col py-5">
+                        <p class="text-center">The Green Chair Project is a non-profit organization that collects donated furnishings for people transitioning from homelessness, crisis or disasters.  </p>
+                        <p class="text-center">We partner with over 55 agencies, shelters and nonprofits to provide essential furnishings for referred clients to live sustainably in their new homes.</p>
+                        <p class="green text-center"> Since our founding in 2010, The Green Chair Project has helped more than 2,300 families begin a new.</p>
                     </div>
                 </div>
             </div>
         </div>
-        
+        <div id="essentials">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 py-5">
+                        <h3 class="text-center">Five Essential Items</h3>
+                    </div>
+                    <div class="essential-item">
+                        <img src="<?php bloginfo('template_directory'); ?>/assets/images/couch.svg" />
+                        <h6>Couches</h6>
+                    </div>
+                    <div class="essential-item">
+                        <img src="<?php bloginfo('template_directory'); ?>/assets/images/loveseat.svg" />
+                        <h6>Love Seats</h6>
+                    </div>
+                    <div class="essential-item">
+                        <img src="<?php bloginfo('template_directory'); ?>/assets/images/tables.svg" />
+                        <h6>Tables</h6>
+                    </div>
+                    <div class="essential-item">
+                        <img src="<?php bloginfo('template_directory'); ?>/assets/images/chairs.svg" />
+                        <h6>Chairs</h6>
+                    </div>
+                    <div class="essential-item">
+                        <img src="<?php bloginfo('template_directory'); ?>/assets/images/dresser.svg" />
+                        <h6>Dressers</h6>
+                    </div>
+                    <div class="col-sm-12 pt-4">
+                        <p>We offer FREE weekly pick-ups in Wake County for the 5 core building blocks of any home. Please fill out our Essentials Pick-Up Donation Form and we’ll be in contact shortly!</p>
+                        <p class="pb-3">Please note, free pick up scheduling is first come, first served. Read more here.</p>
+                        <a href="" class="btn btn-green">Schedule a Free Pickup</a> 
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="home-bottom">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col left">
+                    </div>
+                    <div class="col right">
+                        <h4 class="text-center align-middle">Community support makes new beginnings possible</h4>
+                        <div class="text-center">
+                            <a href="" class="btn btn-primary my-3">Give today</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     <?php endwhile; ?>
     <?php endif; ?>     
 </div><!-- #main-content -->
