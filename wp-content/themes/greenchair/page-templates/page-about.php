a7e8c373f5c12f5a9525f7b15dd8734d3da6d430
daniel <dpipkin@centerline.net>
2019-03-18 15:02:16 -0400

    about page styles

--- wp-content/themes/greenchair/page-templates/page-about.php
+++ wp-content/themes/greenchair/page-templates/page-about.php
@@ -16,31 +16,20 @@ get_header(); ?>
         </div>
     </div>
     <div class="container" style="margin-top: 50px;">
-        <div class="row">
-            <h3 class="col-sm-6 col-md-6 col-lg-6">Working together to provide everyone the comforts of home.</h3>
-            <p class="col-sm-6 col-md-6 col-lg-5">We reuse donated household furnishings to renew lives of participants referred from area programs who are recovering after homelessness, crisis or disaster serving more than 2,300 families in Wake County since 2010.</p>
-        </div>
-    </div>
-    <div class="container" style="margin-top: 30px">
-        <div class="row">
-            <div class="img-placeholder col-sm-6 col-md-4">
-                <img class="img-placeholder col-sm-6 col-md-4" alt="" src="http://placehold.it/400x400" style="max-width: 320px; max-height: 320px;"/>
-            </div>
-            <div class="img-placeholder col-sm-6 col-md-4">
-                <img class="img-placeholder col-sm-6 col-md-4" alt="" src="http://placehold.it/400x400" style="max-width: 320px; max-height: 320px;"/>
-            </div>
-            <div class="img-placeholder col-sm-6 col-md-4">
-                <img class="img-placeholder col-sm-6 col-md-4" alt="" src="http://placehold.it/400x400" style="max-width: 320px; max-height: 320px;"/>
-            </div>
-        </div>
-    </div>
-    <div class="jumbotron d-flex flex-column justify-content-center align-items-center" style="background-image: url(); margin-top: 50px; padding-top: 100px; padding-bottom: 100px;">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-8 ">
-                    <h2>Sweeter Dreams ~</h2>
-                    <h2>A bed for every child</h2>
+        <div class="row doc-wrap">
+            <div class="col-sm-6 col-md-12 col-lg-6">
+                <h3>Working together to provide everyone the comforts of home.</h3>
+                <div class="margin-top-md" style="margin-top: 25px;">
+                    <div class="greenchair-documentary d-flex justify-content-center align-items-center embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/W814_I2slXg" allowfullscreen></iframe>
+                    </div>
                 </div>
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-6 content">
+                <p>We reuse donated household furnishings to renew lives of participants referred from area programs who are recovering after homelessness, crisis or disaster serving more than 2,300 families in Wake County since 2010.</p>
+                <p>The Green Chair Project is a 501(c)(3) that provides home furnishings donated from our community for families and individuals who have transitioned from experiencing homelessness or disasters and have secured sustainable housing.</p>
+                <p>The reusable gifts are cleaned and repaired by volunteers and are presented in a way that promotes dignity and respect. For a small fee, families working with case managers from partnering agencies can select furniture, linens, cookware and accessories to outfit their new homes.</p>
+                <p>The Green Chair experience encourages families to prioritize and make choices and empowers them to create their surroundings and rebuild their lives.</p>
             </div>
         </div>
     </div>
