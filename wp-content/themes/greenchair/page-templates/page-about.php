ad66e7fbd6b9d65c28e758a1ee6d63e6abc2481e
daniel <dpipkin@centerline.net>
2019-03-20 15:19:46 -0400

    adds wisywig hook for story content

--- wp-content/themes/greenchair/page-templates/page-about.php
+++ wp-content/themes/greenchair/page-templates/page-about.php
@@ -33,19 +33,19 @@ get_header(); ?>
         <div class="jumbotron jumbo-breakdown sepia d-flex flex-column justify-content-center align-items-center" style="background-image: url(http://greenchair.loc:8888/wp-content/uploads/2019/03/impact.jpg);">
             <div class="container">
                 <div class="column">
-                    <h1>Impact</h1>
+                    <h1><?php echo get_field('impact_heading'); ?></h1>
                     <div class="d-flex col-xs-12 col-sm-6 col-md-6">
                         <div class="breakdown-block">
-                            <h2>9</h2>
-                            <p>years</p>
+                            <h2><?php echo get_field('impact_top_first'); ?></h2>
+                            <p><?php echo get_field('impact_bottom_first'); ?></p>
                         </div>
                         <div class="breakdown-block">
-                            <h2>55</h2>
-                            <p>volunteers</p>
+                            <h2><?php echo get_field('impact_top_second'); ?></h2>
+                            <p><?php echo get_field('impact_bottom_second'); ?></p>
                         </div>
                         <div class="breakdown-block">
-                            <h2>2300+</h2>
-                            <p>families served</p>
+                            <h2><?php echo get_field('impact_top_third'); ?></h2>
+                            <p><?php echo get_field('impact_bottom_third'); ?></p>
                         </div>
                     </div>
                 </div>
@@ -54,10 +54,8 @@ get_header(); ?>
         <div class="container" style="margin-top: 50px;">
             <div class="row">
                 <div class="col-sm-12 col-md-6 col-lg-7 content">
-                    <h2>Our story</h2>
-                    <p>Both trained in real estate home staging, Jackie Craig and Beth Smoot consistently found that homeowners had too much stuff in their houses  – stuff they didn’t want to go to waste but no longer had a need for.</p>
-                    <p>After seeing this again and again, the pair founded The Green Chair Project, Inc. in 2010 as a receptacle for those items, as well as a “green” way to reuse good stuff to benefit their neighbors in need.</p>
-                    <p style="font-style: italic;">Pictured <span class="picture-direction"></span>: Beth Smoot (Left) and Jackie Craig (Right), Co-Founders of The Green Chair Project</p>            
+                    <h2><?php echo get_field('story_heading'); ?></h2>
+                    <?php the_field('about_story_section'); ?>
                 </div>
                 <div class="col-sm-12 col-md-6 col-lg-5 content">
                     <div class="founder-img" style="background-image: url(http://greenchair.loc:8888/wp-content/uploads/2019/03/25299861758_7ea43c5a40_z.jpg);"></div>
