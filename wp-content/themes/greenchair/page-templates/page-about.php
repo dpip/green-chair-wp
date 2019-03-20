9045045a3afb42e06b13ea31c0d6d0ceab88b463
daniel <dpipkin@centerline.net>
2019-03-20 14:00:34 -0400

    adds stock imagery // padding and styles for jumbotrons // our story title

--- wp-content/themes/greenchair/page-templates/page-about.php
+++ wp-content/themes/greenchair/page-templates/page-about.php
@@ -6,10 +6,10 @@ get_header(); ?>
 
 <div id="main-content" class="about-page">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <div class="jumbotron d-flex flex-column justify-content-center align-items-center" style="background-image: url(<?php echo $backgroundimage ?>);">
+        <div class="jumbotron jumbo-about d-flex flex-column justify-content-center align-items-center" style="background-image: url(http://greenchair.loc:8888/wp-content/uploads/2019/03/greenchair-2.jpg);">
             <div class="container">
                 <div class="row">
-                    <div class="col-xs-12 col-sm-12 col-md-8 ">
+                    <div class="col-xs-12 col-sm-12 col-md-12 ">
                         <h1 class="my-3 display-3"><?php echo get_the_title(); ?></h1>
                     </div>
                 </div>
@@ -30,7 +30,7 @@ get_header(); ?>
                 </div>
             </div>
         </div>
-        <div class="jumbotron jumbo-breakdown d-flex flex-column justify-content-center align-items-center" style="background-image: url(<?php echo $backgroundimage ?>);">
+        <div class="jumbotron jumbo-breakdown sepia d-flex flex-column justify-content-center align-items-center" style="background-image: url(http://greenchair.loc:8888/wp-content/uploads/2019/03/impact.jpg);">
             <div class="container">
                 <div class="column">
                     <h1>Impact</h1>
@@ -53,13 +53,14 @@ get_header(); ?>
         </div>
         <div class="container" style="margin-top: 50px;">
             <div class="row">
-                <div class="col-sm-12 col-md-8 col-lg-8 content">
+                <div class="col-sm-12 col-md-6 col-lg-7 content">
+                    <h2>Our story</h2>
                     <p>Both trained in real estate home staging, Jackie Craig and Beth Smoot consistently found that homeowners had too much stuff in their houses  – stuff they didn’t want to go to waste but no longer had a need for.</p>
                     <p>After seeing this again and again, the pair founded The Green Chair Project, Inc. in 2010 as a receptacle for those items, as well as a “green” way to reuse good stuff to benefit their neighbors in need.</p>
                     <p style="font-style: italic;">Pictured <span class="picture-direction"></span>: Beth Smoot (Left) and Jackie Craig (Right), Co-Founders of The Green Chair Project</p>            
                 </div>
-                <div class="col-sm-12 col-md-4 col-lg-4 content">
-                    <div class="founder-img" style="background-image: url(https://dummyimage.com/600x400/000/fff);"></div>
+                <div class="col-sm-12 col-md-6 col-lg-5 content">
+                    <div class="founder-img" style="background-image: url(http://greenchair.loc:8888/wp-content/uploads/2019/03/25299861758_7ea43c5a40_z.jpg);"></div>
                 </div>
             </div>
         </div>
