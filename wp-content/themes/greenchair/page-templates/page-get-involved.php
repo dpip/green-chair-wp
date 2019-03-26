b816c75a4e2abc2c27a19a4a9dcd3a78d7eff93c
Rebecca Elmo <belmo@centerline.net>
2019-03-26 12:44:46 -0400

    updating the get involved page

--- wp-content/themes/greenchair/page-templates/page-get-involved.php
+++ wp-content/themes/greenchair/page-templates/page-get-involved.php
@@ -20,52 +20,52 @@ get_header(); ?>
         <div id="get-involved-types">
             <div class="container">
                 <div class="row">
-                    <div class="type col-xs-12 col-sm-3">
+                    <div class="type col-xs-12 col-lg-3">
                         <div class="inner">
                             <div class="img-wrap">
-                                <a href="">
-                                    <img src="/wp-content/uploads/2019/03/volunteerhands.jpg" class="img-fluid d-block mx-auto" />
+                                <a href="<?php the_field('get_involved_box_1_cta_url'); ?>">
+                                    <img src="<?php the_field('get_involved_box_1_image'); ?>" class="img-fluid d-block mx-auto" />
                                 </a>
                             </div>
-                            <h3>Volunteer</h3>
-                            <p>The Green Chair is a volunteer-driven operation, so we rely on volunteers in all areas. Check out our volunteer page for opportunities.</p>
-                            <a href="/volunteer" class="btn btn-primary">Join Our Team</a>
+                            <h3><?php the_field('get_involved_box_1'); ?></h3>
+                            <p><?php the_field('get_involved_box_1_paragraph'); ?></p>
+                            <a href="<?php the_field('get_involved_box_1_cta_url'); ?>" class="btn btn-primary"><?php the_field('get_involved_box_1_cta_text'); ?></a>
                         </div>
                     </div>
-                    <div class="type col-xs-12 col-sm-3">
+                    <div class="type col-xs-12 col-lg-3">
                         <div class="inner">
                             <div class="img-wrap">
-                                <a href="">
-                                    <img src="/wp-content/uploads/2019/03/kelly-miller-392332-unsplash.jpg" class="img-fluid d-block mx-auto" />
+                                <a href="<?php the_field('get_involved_box_2_cta_url'); ?>">
+                                    <img src="<?php the_field('get_involved_box_2_image'); ?>" class="img-fluid d-block mx-auto" />
                                 </a>
                             </div>
-                            <h3>Donate Furnishings</h3>
-                            <p>We need and accept gently-used household furnishings donations. Click here to see our donation hours and a list of what we accept.</p>
-                            <a href="/donate-furnishings" class="btn btn-primary">Learn more</a>
+                            <h3><?php the_field('get_involved_box_2'); ?></h3>
+                            <p><?php the_field('get_involved_box_2_paragraph'); ?></p>
+                            <a href="<?php the_field('get_involved_box_2_cta_url'); ?>" class="btn btn-primary"><?php the_field('get_involved_box_2_cta_text'); ?></a>
                         </div>
                     </div>
-                    <div class="type col-xs-12 col-sm-3">
+                    <div class="type col-xs-12 col-lg-3">
                         <div class="inner">
                             <div class="img-wrap">
-                                <a href="">
-                                    <img src="/wp-content/uploads/2019/03/donate.jpg" class="img-fluid d-block mx-auto" />
+                                <a href="<?php the_field('get_involved_box_3_cta_url'); ?>">
+                                    <img src="<?php the_field('get_involved_box_3_image'); ?>" class="img-fluid d-block mx-auto" />
                                 </a>
                             </div>
-                            <h3>Financial Contribution</h3>
-                            <p>Whether you make a one time contribution or you can help as a monthly sustainer, your financial support is critical! </p>
-                            <a href="/donate" class="btn btn-primary">Give today</a>
+                            <h3><?php the_field('get_involved_box_3'); ?></h3>
+                            <p><?php the_field('get_involved_box_3_paragraph'); ?></p>
+                            <a href="<?php the_field('get_involved_box_3_cta_url'); ?>" class="btn btn-primary"><?php the_field('get_involved_box_3_cta_text'); ?></a>
                         </div>
                     </div>
-                    <div class="type col-xs-12 col-sm-3">
+                    <div class="type col-xs-12 col-lg-3">
                         <div class="inner">
                             <div class="img-wrap">
-                                <a href="">
-                                    <img src="/wp-content/uploads/2019/03/corporatesponsorship.jpg" class="img-fluid d-block mx-auto" />
+                                <a href="<?php the_field('get_involved_box_4_cta_url'); ?>">
+                                    <img src="<?php the_field('get_involved_box_4_image'); ?>" class="img-fluid d-block mx-auto" />
                                 </a>
                             </div>
-                            <h3>Corporate Sponsorship</h3>
-                            <p>The Green Chair Project welcomes corporate sponsors which share its mission of encouraging reuse and renewal.</p>
-                            <a href="/corporate-sponsorship" class="btn btn-primary">Sustain Our Work</a>
+                            <h3><?php the_field('get_involved_box_4'); ?></h3>
+                            <p><?php the_field('get_involved_box_4_paragraph'); ?></p>
+                            <a href="<?php the_field('get_involved_box_4_cta_url'); ?>" class="btn btn-primary"><?php the_field('get_involved_box_4_cta_text'); ?></a>
                         </div>
                     </div>
                 </div>
@@ -75,19 +75,19 @@ get_header(); ?>
             <div class="container">
                 <div class="row">
                     <div class="col-sm-12 pt-3">
-                        <h3 class="py-5 text-center">Other ways to help</h3>
+                        <h3 class="py-5 text-center"><?php the_field('second_heading'); ?></h3>
                     </div>
-                    <div class="col-sm-4">
-                        <h6>Fundraiser/Furniture Sales</h6>
-                        <p>Visit our booths at these stores to purchase high-end Green Chair items. <a href="/fundraiser-sales">Learn more &raquo; </a></p>
+                    <div class="col-lg-4">
+                        <h6><?php the_field('other_ways_box_1'); ?></h6>
+                        <p><?php the_field('other_ways_box_1_paragraph'); ?> <a href="<?php the_field('other_ways_box_1_url'); ?>">Learn more &raquo; </a></p>
                     </div>
-                    <div class="col-sm-4">
-                        <h6>Shop our Online Store</h6>
-                        <p>Browse our high-end and vintage items in our online store. <a href="https://www.chairish.com/shop/thegreenchairproject">Learn more &raquo;</a></p>
+                    <div class="col-lg-4">
+                        <h6><?php the_field('other_ways_box_2'); ?></h6>
+                        <p><?php the_field('other_ways_box_2_paragraph'); ?> <a href="<?php the_field('other_ways_box_2_url'); ?>">Learn more &raquo; </a></p>
                     </div>
-                    <div class="col-sm-4">
-                        <h6>Charity Event</h6>
-                        <p>Attend our Charity Event Gala this April to support Green Chair. <a href="/events-item/charity-2019/">Learn more &raquo; </a></p>
+                    <div class="col-lg-4">
+                        <h6><?php the_field('other_ways_box_3'); ?></h6>
+                        <p><?php the_field('other_ways_box_3_paragraph'); ?> <a href="<?php the_field('other_ways_box_3_url'); ?>">Learn more &raquo; </a></p>
                     </div>
                 </div>
             </div>
@@ -96,20 +96,63 @@ get_header(); ?>
             <div class="container">
                 <div class="row">
                     <div class="col-sm-12 pt-3 text-center py-2">
-                        <h4 class="py-2">Our Supporters</h4>
-                        <p>A BIG thank you those companies and organizations who help support the community through donations to the Green Chair Project!</p>
+                        <h4 class="py-2"><?php the_field('supporters_heading'); ?></h4>
+                        <p><?php the_field('supporters_text'); ?></p>
                     </div>
                 </div>
             </div>
         </div>
         <div id="supporters-list">
             <div class="container">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <h3 class="text-center mt-4 mb-3 underline">$10,000+</h3>
-                    </div>
-                    
-                    
+                <div class="row">                    
+                    <?php
+                        $terms = get_terms( array( 'taxonomy' => 'supporter_category', 'hide_empty' => false) );
+
+                        if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) {
+                            foreach ( $terms as $term ) {
+                                echo '<div class="col-sm-12">';
+                                    echo '<h3 class="text-center mt-4 mb-3 underline">'. $term->name .'</h3>';
+                                echo '</div>';
+
+                                $args = array( 
+                                    'post_type' => 'supporters', 
+                                    'posts_per_page' => -1, 
+                                    'tax_query' => array(
+                                        array(
+                                            'taxonomy' => 'supporter_category',
+                                            'field'    => 'slug',
+                                            'terms'    => $term->slug
+                                        )
+                                    )
+                                );
+
+                                $the_query = new WP_Query($args);
+
+                                if ( $the_query->have_posts() ) {
+                                    while ( $the_query->have_posts() ) {
+                                        $the_query->the_post();
+
+                                        $id = get_the_ID();
+                                        $img = get_the_post_thumbnail($id, 'full', array('class' => 'img-fluid d-block mx-auto'));
+                                        $title = get_the_title($id);
+
+                                        echo '<div class="col-lg-3 my-2">';
+                                            if(!empty($img)){
+                                                echo $img;
+                                            }else{
+                                                echo '<p>'. $title . '</p>';
+                                            }
+                                        echo '</div>';
+                                    }
+                                    wp_reset_postdata();
+                                } else {
+                                    echo '<p>No supporters in this category.</p>';
+                                }
+
+
+                            }
+                        }
+                    ?>
                 </div>
             </div>
         </div>
