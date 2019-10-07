864b74c4c618efc5b0cf27d66681a31b64121f78
daniel <dpipkin@centerline.net>
2019-10-07 17:06:35 -0400

    adds boxes with custom fields to volunteer page

--- wp-content/themes/greenchair/page-templates/page-volunteer.php
+++ wp-content/themes/greenchair/page-templates/page-volunteer.php
@@ -18,10 +18,42 @@ get_header(); ?>
         </div>
         <div id="volunteer-info" class="container">
             <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-8 py-5">
+                <div class="col-xs-12 col-sm-12 col-md-12 py-5">
                     <?php the_content(); ?>
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-12 col-md-4 py-5">
+                            <div class="card">
+                            <img class="box-image" style="height: 225px;" src="<?php the_field('volunteer_box_image_1'); ?>" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title teal"><?php the_field('volunteer_box_title_1'); ?></h5>
+                                <p class="card-text"><?php the_field('volunteer_box_content_1'); ?></p>
+                                <a href="<?php the_field('volunteer_box_cta_url_1'); ?>" class="btn btn-primary"><?php the_field('volunteer_box_cta_1'); ?></a>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-4 py-5">
+                            <div class="card">
+                            <img class="box-image" src="<?php the_field('volunteer_box_image_2'); ?>" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title teal"><?php the_field('volunteer_box_title_2'); ?></h5>
+                                <p class="card-text"><?php the_field('volunteer_box_content_2'); ?></p>
+                                <a href="<?php the_field('volunteer_box_cta_url_2'); ?>" class="btn btn-primary"><?php the_field('volunteer_box_cta_2'); ?></a>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-4 py-5">
+                            <div class="card">
+                            <img class="box-image" src="<?php the_field('volunteer_box_image_3'); ?>" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title teal"><?php the_field('volunteer_box_title_3'); ?></h5>
+                                <p class="card-text"><?php the_field('volunteer_box_content_3'); ?></p>
+                                <a href="<?php the_field('volunteer_box_cta_url_3'); ?>" class="btn btn-primary"><?php the_field('volunteer_box_cta_3'); ?></a>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-xs-12 col-sm-12 col-md-4 py-5">
+                <!-- <div class="col-xs-12 col-sm-12 col-md-4 py-5">
                     <div class="news-item">
                         <div class="block-content">
                             <a class="continue float-right" href="<?php the_field('sidebar_cta_url_1'); ?>">
@@ -29,7 +61,7 @@ get_header(); ?>
                             </a>
                             <h5 class="font-weight-bold"><?php the_field('sidebar_title_1'); ?></h5>
                             <p class="content"><?php the_field('sidebar_copy_1'); ?></p>
-                            <a class="continue float-right" href="<?php the_field('sidebar_cta_url_1'); ?>"><?php the_field('sidebar_cta_text_1'); ?> &raquo; </a>
+                            <a class="continue float-right" href="<?php the_field('volunteer_box_cta_url_1'); ?>"><?php the_field('sidebar_cta_text_1'); ?> &raquo; </a>
                             <div class="clearfix"></div>
                         </div>
                     </div>
@@ -44,7 +76,7 @@ get_header(); ?>
                             <div class="clearfix"></div>
                         </div>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
     <?php endwhile; ?>
