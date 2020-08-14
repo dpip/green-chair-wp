9a47762faee5084ef5df6ea7502c293fcfb59b88
Daniel Pipkin <danielpipkin@Centerlines-MacBook-Pro.local>
2020-08-14 15:16:50 -0400

    adds currenxy db export to root of directory aka dbdumb // footer scaffolded

--- wp-content/themes/greenchair/footer.php
+++ wp-content/themes/greenchair/footer.php
@@ -1,42 +1,65 @@
             </div><!--main-->
         </div><!-- wrapper-->
-        <div id="footer" class="py-4">
-            <div class="container">
+        <div id="footer">
+            <div class="container" style="padding-top: 2rem; padding-bottom: 2rem;">
                 <div class="row">
                     <div class="col-sm footer-contact-col">
-                        <h6>Contact Us:</h6>
-                        <p><span>1853 Capital Boulevard, Raleigh, NC 27604<br /> <a href="tel:(919) 322-0474 ">(919) 322-0474</a>&nbsp;|&nbsp;<a href="mailto:hello@thegreenchair.org">hello@thegreenchair.org</a></span></p>
+                        <h6 style="color: #363f44; text-transform: uppercase; font-weight: bold; font-size: 1.5rem;">contact us</h6>
+                        <p class="footer--contact-item" >1853 Capital Boulevard,<br>Raleigh, NC 27604</p>
+                        
+                        <a class="footer--contact-item" href="tel:(919) 322-0474 ">(919) 322-0474</a>
+                        <a class="footer--contact-item footer--contact-email" href="mailto:hello@thegreenchair.org">hello@thegreenchair.org</a>
+                        <!-- <p><span>1853 Capital Boulevard, Raleigh, NC 27604<br /> <a href="tel:(919) 322-0474 ">(919) 322-0474</a>&nbsp;|&nbsp;<a href="mailto:info@thegreenchair.org">info@thegreenchair.org</a></span></p> -->
+                        <!-- <p>Registered 501(c)(3) Charity: 27-2323103</p>
+                        <p>Copyright &copy; <?php echo date('Y'); ?> | <a href="/careers">Careers</a> | <a href="/privacy-policy">Privacy Policy</a></p> -->
+                    </div>
+                    <div class="col-sm" id="creds">
+                        <img style="width: 135px; height: 135px;" src="<?php bloginfo('template_directory'); ?>/assets/images/nccenterfornonprofits.svg" class="img-fluid" />
+                        <img style="width: 135px; height: 135px;" src="<?php bloginfo('template_directory'); ?>/assets/images/sealtransparency.png" class="img-fluid" />
+                        <img style="width: 175px; height: 100px;" src="<?php bloginfo('template_directory'); ?>/assets/images/tbjaward.png" class="img-fluid" />
+                    </div>
+                    <div class="col-sm footer-sign-up-col">
+                    <!--TODO: CODE neded -->
+                    <h6 style="color: #363f44; text-transform: uppercase; font-weight: bold; font-size: 1.5rem;">keep in touch</h6>
+                        
+                        <div class="footer-input-container">
+                            <input id="footer-sign-up-field" type="text-area" placeholder="Email address"></input>
+                            <a class="btn btn-green">Subscribe</a>
+                        </div>
                         <ul id="social-links">
                             <li><a href="https://www.facebook.com/thegreenchairproject" target="_blank"><i class="fab fa-facebook-square"></i></a></li>
                             <li><a href="https://twitter.com/The_Green_Chair" target="_blank"><i class="fab fa-twitter-square"></i></a></li>
                             <li><a href="https://www.instagram.com/greenchairproject/" target="_blank"><i class="fab fa-instagram"></i></a></li>
                             <li><a href="https://www.linkedin.com/company/greenchairproject/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
                         </ul>
-                        <!-- <p><span>1853 Capital Boulevard, Raleigh, NC 27604<br /> <a href="tel:(919) 322-0474 ">(919) 322-0474</a>&nbsp;|&nbsp;<a href="mailto:info@thegreenchair.org">info@thegreenchair.org</a></span></p> -->
-                        <p>Registered 501(c)(3) Charity: 27-2323103</p>
-                        <p>Copyright &copy; <?php echo date('Y'); ?> | <a href="/careers">Careers</a> | <a href="/privacy-policy">Privacy Policy</a></p>
-                    </div>
-                    <div class="col-sm" id="creds">
-                        <img src="<?php bloginfo('template_directory'); ?>/assets/images/nccenterfornonprofits.svg" class="img-fluid" />
-                        <img src="<?php bloginfo('template_directory'); ?>/assets/images/sealtransparency.png" class="img-fluid" />
-                        <img src="<?php bloginfo('template_directory'); ?>/assets/images/tbjaward.png" class="img-fluid" />
-                    </div>
-                    <div class="col-sm">
-                    <!--TODO: CODE neded -->
-                        <p><a href="/">Newsletter Sign Up Form</a></p>
-
-                        
-                        <div class="ctct-inline-form" data-form-id="XXXXXXXX-5ede-4802-8f87-cf552f6cd7cf"></div>
+                        <!-- <div class="ctct-inline-form" data-form-id="XXXXXXXX-5ede-4802-8f87-cf552f6cd7cf"></div>
                         <p><a href="/careers">Careers</a></p>
-                        <p><a href="/financials">Financials</a></p>
+                        <p><a href="/financials">Financials</a></p> -->
                     </div>
                 </div>
             </div>
+            <div class="footer-legal">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm">
+                            <p class="copyright">The Green Chair is a 501 (c)(3) not-for-profit organization. EIN number: 27-2323103<br>Copyright © The Green Chair, 2020</p>
+                        </div>
+                        <div class="col-sm footer-legal-links">
+                        <p><a class="legal-link" href="/careers">Careers</a></p>
+                        <div class="legal-divider">|</div>
+                        <p><a class="legal-link" href="/financials">Financials</a></p>
+                        <div class="legal-divider">|</div>
+                        <p><a class="legal-link" href="/privacy">Privacy</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
         </div>
         <?php wp_footer(); ?>
         <script src="<?php bloginfo('template_directory'); ?>/assets/js/modernizr.js"></script>
         <script src="<?php bloginfo('template_directory'); ?>/assets/js/main.js"></script>
-        <script> var _ctct_m = "3abb8c8516c6fc547b50ecde646e13a3";</script>
-        <script id="signupScript" src="//static.ctctcdn.com/js/signup-form-widget/current/signup-form-widget.min.js" async defer></script>
+        <!-- <script> var _ctct_m = "3abb8c8516c6fc547b50ecde646e13a3";</script>
+        <script id="signupScript" src="//static.ctctcdn.com/js/signup-form-widget/current/signup-form-widget.min.js" async defer></script> -->
     </body>
 </html>
